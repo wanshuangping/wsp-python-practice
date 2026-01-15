20b270fce4cba3e75353888a69542c44b1034c2d
wanshuangping <1931719950@qq.com>
2026-01-14 21:49:09 -0800

    写了一套小代码把所有的开始时间和结束时间都批量填写了填写备注

--- tests/fill_data.py
+++ tests/fill_data.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import pandas as pd  # 用这个库存来操作
 
-csv_path = "../data/exercise_calories.csv"
-df = pd.read_csv(csv_path)
-df['start_time'] = [i * 10 for i in range(len(df))]
+csv_path = "../data/exercise_calories.csv"  # 告诉需要操作的文件路径
+df = pd.read_csv(csv_path)   # 用这个方法读取
+df['start_time'] = [i * 10 for i in range(len(df))]  # 需要添加的列名,做个循环
 df['end_time'] = [(i + 1) * 10 for i in range(len(df))]
-df.to_csv(csv_path,index=False,encoding='utf-8')
+df.to_csv(csv_path,index=False,encoding='utf-8') # 输出
 print("success,fill in")
