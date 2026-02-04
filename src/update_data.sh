d0392a1e136e0c8e9c2b47784af8e28f6cc39211
wanshuangping <1931719950@qq.com>
2026-02-03 21:50:59 -0800

    股票图-数据库-ai预测

--- src/update_data.sh
+++ src/update_data.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# 1. 进入你的项目根目录
 cd /Users/temu/PycharmProjects/pythonProject
-
-# 2. 使用项目自带的虚拟环境运行数据库同步脚本
-# 这样可以确保所有依赖包（如 akshare, sqlite3）都能被正确加载
 ./.venv/bin/python src/crawler/data_storage.py >> logs/auto_update.log 2>&1
