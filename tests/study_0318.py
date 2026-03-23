0eacbbc7eecee7979c491c8fa09e79a5dec0ef11
wanshuangping <1931719950@qq.com>
2026-03-22 19:01:53 -0700

    STUDY0323

--- tests/study_0318.py
+++ tests/study_0318.py
@@ -85,4 +85,8 @@ total = [item_01,item_02,item_03]
 # str = '12345678'
 # print(str[:3])
 # input('\n\n按下 enter 键后退出')
-import sys;x = 'roy';sys.stdout.write(x+'\n')
+# import sys;x = 'roy';sys.stdout.write(x+'\n')
+y = 'a'
+x = 'b'
+print(y)
+print(x)
