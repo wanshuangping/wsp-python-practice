71059e0807c61347da389385e72bbb287b692052
wanshuangping <1931719950@qq.com>
2026-04-09 03:08:25 -0700

    0409test

--- tests/test_04.py
+++ tests/test_04.py
@@ -1,9 +1,16 @@
-def reverseWords(input_str):
-    inputWords = input_str.split()
-    inputWords = inputWords[-1::-1]
-    output = ''.join(inputWords)
-    return output
-if __name__ == '__main__':
-    input_str = 'i leke rob'
-    rw = reverseWords(input_str)
-    print(rw)
+# def reverseWords(input_str):
+#     inputWords = input_str.split()
+#     inputWords = inputWords[-1::-1]
+#     output = ''.join(inputWords)
+#     return output
+# if __name__ == '__main__':
+#     input_str = 'i leke rob'
+#     rw = reverseWords(input_str)
+#     print(rw)
+tuple = ('abc',123,2.3,'roy',70.1)
+tinytuple = (123,'roy')
+print(tuple)
+print(tuple[0])
+print(tuple[2:])
+print(tinytuple * 2)
+print(tuple + tinytuple)
