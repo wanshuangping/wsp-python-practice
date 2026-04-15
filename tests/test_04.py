966deb70834757dac1463e0a3be98223f06f7cce
wanshuangping <1931719950@qq.com>
2026-04-15 03:08:16 -0700

    0415test

--- tests/test_04.py
+++ tests/test_04.py
@@ -17,6 +17,11 @@
 # tup = (1,2,3,4,5,6,7)
 # # tup[0] = 11(不能直接对元祖元素修改会报语法错误)
 # print(tup[0])
-tup1 = ()
-tup2 = (20, )
-print(tup1,tup2)
+# tup1 = ()
+# tup2 = (20, )
+# print(tup1,tup2)
+sets = {'baidu','xiaomi','1688'}
+if '1688' in sets:
+    print('1688 is sets now')
+else:
+    print('1688 not in set')
